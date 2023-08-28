9de2c5d47f0357a030e1e9f4c769c22ae8762a81
abdoneverland <ch.abdel.news@gmail.com>
2023-08-28 21:59:36 +0100

    task 6

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder -  in-order traversal
+ * @tree: tree
+ * @func: function to apply
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (!tree)
+	return;
+binary_tree_preorder(tree->left, func);
+func(tree->n);
+binary_tree_preorder(tree->right, func);
+}
